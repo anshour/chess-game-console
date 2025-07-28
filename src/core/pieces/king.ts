d33e2a7427a21d20f07167b5efc78455d9157286
Anshor J <anshorje@gmail.com>
2025-07-28 16:43:10 +0700

    Add castling moves for king

--- src/core/pieces/king.ts
+++ src/core/pieces/king.ts
@@ -8,28 +8,53 @@ export class King extends Piece {
     super(color, PieceType.KING, position);
   }
 
-  getMovementMoves(board: Board): Position[] {
+  private getCastlingMoves(board: Board): Position[] {
     const moves: Position[] = [];
-    const directions = [
-      [1, 0],   // up
-      [-1, 0],  // down
-      [0, 1],   // right
-      [0, -1],  // left
-      [1, 1],   // up-right
-      [1, -1],  // up-left
-      [-1, 1],  // down-right
-      [-1, -1]  // down-left
-    ];
+    if (this.hasMoved || board.isKingInCheck(this.color)) {
+      return [];
+    }
 
-    for (const [rankDir, fileDir] of directions) {
-      const newRank = this.position.rankIndex + rankDir;
-      const newFile = this.position.fileIndex + fileDir;
+    const rank = this.position.rankIndex;
+    const opponentColor =
+      this.color === PieceColor.WHITE ? PieceColor.BLACK : PieceColor.WHITE;
 
-      if (board.areCoordinatesWithinBoard(newRank, newFile)) {
-        const newPosition = new Position(newRank, newFile);
+    // 1. Rokade sisi Raja (Kingside, O-O)
+    const kingsideRook = board.getPieceAt(new Position(rank, 7));
+    if (
+      kingsideRook &&
+      kingsideRook.type === PieceType.ROOK &&
+      !kingsideRook.hasMoved
+    ) {
+      const pathClear =
+        !board.getPieceAt(new Position(rank, 5)) &&
+        !board.getPieceAt(new Position(rank, 6));
+      if (pathClear) {
+        const pathSafe =
+          !board.isSquareAttacked(new Position(rank, 5), opponentColor) &&
+          !board.isSquareAttacked(new Position(rank, 6), opponentColor);
+        if (pathSafe) {
+          moves.push(new Position(rank, 6));
+        }
+      }
+    }
 
-        if (this.isEmpty(newPosition, board)) {
-          moves.push(newPosition);
+    // 2. Rokade sisi Ratu (Queenside, O-O-O)
+    const queensideRook = board.getPieceAt(new Position(rank, 0));
+    if (
+      queensideRook &&
+      queensideRook.type === PieceType.ROOK &&
+      !queensideRook.hasMoved
+    ) {
+      const pathClear =
+        !board.getPieceAt(new Position(rank, 1)) &&
+        !board.getPieceAt(new Position(rank, 2)) &&
+        !board.getPieceAt(new Position(rank, 3));
+      if (pathClear) {
+        const pathSafe =
+          !board.isSquareAttacked(new Position(rank, 2), opponentColor) &&
+          !board.isSquareAttacked(new Position(rank, 3), opponentColor);
+        if (pathSafe) {
+          moves.push(new Position(rank, 2));
         }
       }
     }
@@ -37,17 +62,17 @@ export class King extends Piece {
     return moves;
   }
 
-  getAttackMoves(board: Board): Position[] {
+  private getBasicMoves(board: Board): Position[] {
     const moves: Position[] = [];
     const directions = [
-      [1, 0],   // up
-      [-1, 0],  // down
-      [0, 1],   // right
-      [0, -1],  // left
-      [1, 1],   // up-right
-      [1, -1],  // up-left
-      [-1, 1],  // down-right
-      [-1, -1]  // down-left
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
     ];
 
     for (const [rankDir, fileDir] of directions) {
@@ -56,13 +81,27 @@ export class King extends Piece {
 
       if (board.areCoordinatesWithinBoard(newRank, newFile)) {
         const newPosition = new Position(newRank, newFile);
-
-        if (this.isEnemyPiece(newPosition, board)) {
-          moves.push(newPosition);
-        }
+        moves.push(newPosition);
       }
     }
 
     return moves;
+  }
+
+  getMovementMoves(board: Board): Position[] {
+    const moves = this.getBasicMoves(board).filter((pos) =>
+      this.isEmpty(pos, board),
+    );
+
+    const castlingMoves = this.getCastlingMoves(board);
+    moves.push(...castlingMoves);
+
+    return moves;
+  }
+
+  getAttackMoves(board: Board): Position[] {
+    return this.getBasicMoves(board).filter((pos) =>
+      this.isEnemyPiece(pos, board),
+    );
   }
 }
